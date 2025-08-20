215be18e68c5b5bad7b2ca2364813b7d4af3c723
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-08-20 22:00:45 +0200

    vite + React + tailwindcss

--- src/Component/Navbar/CountryDrobDown/CountryDrobDown.jsx
+++ src/Component/Navbar/CountryDrobDown/CountryDrobDown.jsx
@@ -6,7 +6,7 @@ export default function CountryDrobDown() {
         <div className="relative flex cursor-pointer group h-full border-1">
             <a href="#" className="px-2 py-1">{country}</a>
 
-            <div className="absolute left-1/2 -translate-x-1/2 top-full hidden group-hover:block bg-white shadow-lg rounded-2xl py-2 transition-all ease-in-out duration-500">
+            <div className="absolute left-1/2 -translate-x-1/2 top-full h-0 overflow-hidden group-hover:h-64 group-hover:py-2 bg-white shadow-lg rounded-2xl transition-all ease-in-out duration-500">
                 <span
                     className="block px-4 py-2 hover:bg-gray-100 cursor-pointer transition-colors ease-in-out duration-300"
                     onClick={() => setCountry("Egypt")}
