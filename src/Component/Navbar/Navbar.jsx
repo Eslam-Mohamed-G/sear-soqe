b8517ac7d0bc6624bdd9503c13ae6b45a82c9a4c
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-08-21 11:14:49 +0200

    add links to navbar with translation file

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -3,20 +3,51 @@ import Logo from '../../assets/logo.png'
 import CountryDrobDown from './CountryDrobDown/CountryDrobDown'
 import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher'
 import { useTranslation } from "react-i18next";
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     const { t } = useTranslation("navbar");
     return (
         <nav className='bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600'>
-            <div className="max-w-screen-xl flex flex-wrap gap-4 items-center mx-auto py-4">
-                <a href="#" className='flex justify-center items-center gap-2 text-[#4044C9]'>
-                    <img src={Logo} alt='logo' />
-                    <p className='font-extrabold text-2xl'><span className='text-[#20BB74]'>{t("logo.firstName")} </span><span className='text-[#4044C9]'>{t("logo.scondName")}</span></p>
-                </a>
+            <div className='flex flex-row flex-wrap items-center justify-between p-4 xl:px-24 font-bold'>
+                {/* avatar && logo */}
+                <div className="flex gap-4 items-center">
+                    <Link to="#" className='flex justify-center items-center gap-1 text-[#4044C9]'>
+                        <img src={Logo} alt='logo' />
+                        <p className='font-extrabold text-2xl'><span className='text-[#20BB74]'>{t("logo.firstName")} </span><span className='text-[#4044C9]'>{t("logo.scondName")}</span></p>
+                    </Link>
 
-                <div className="hidden xl:flex items-center justify-center gap-4 bg-transparent">
-                    <CountryDrobDown/>
-                    <LanguageSwitcher/>
+                    <div className="hidden xl:flex items-center justify-center gap-4 bg-transparent">
+                        <CountryDrobDown />
+                        <LanguageSwitcher />
+                    </div>
+                </div>
+
+                {/* links */}
+                <div className="hidden lg:flex text-black">
+                    <ul className='flex flex-wrap flex-row gap-10'>
+                        <li>
+                            <Link to="#">{t("links.offers")}</Link>
+                        </li>
+                        <li>
+                            <Link to="#">{t("links.new")}</Link>
+                        </li>
+                        <li>
+                            <Link to="#">{t("links.used")}</Link>
+                        </li>
+                        <li>
+                            <Link to="#">{t("links.electric")}</Link>
+                        </li>
+                        <li>
+                            <Link to="#">{t("links.chinese")}</Link>
+                        </li>
+                        <li>
+                            <Link to="#">{t("links.blog")}</Link>
+                        </li>
+                        <li>
+                            <Link to="#">{t("links.services")}</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
