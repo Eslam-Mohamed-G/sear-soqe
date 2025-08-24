4df53da77bc6604d3b98052d608a0faff4ba7e0e
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-08-24 13:50:57 +0200

    add car brand component to BuyCar page

--- src/pages/BuyCar/BuyCar.jsx
+++ src/pages/BuyCar/BuyCar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import bgImg from '../../assets/image.png';
 import DropdownInput from '../../Component/InputDropDown/InputDropDown';
+import CarBrands from '../../Component/CarBrands/CarBrands';
 
 export default function BuyCar() {
     return (
@@ -87,116 +88,7 @@ export default function BuyCar() {
 
                             {/* محتوي التاب بتاعت الماركة */}
                             <div className="w-full">
-                                <div className="grid grid-cols-4 gap-2 mt-4">
-                                    {/* div1 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>هيونداي</span>
-                                            <div className="max-w-16 max-h-16">
-                                                <img src="./car brands/hyundai.png" alt="hyundai car" className='object-contain' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* div2 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>تويوتا</span>
-                                            <div className="max-w-16 max-h-16">
-                                                <img src="./car brands/tyota.png" alt="tyota car" className='object-contain' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* div3 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>شفرولية</span>
-                                            <div className="max-w-16 max-h-16">
-                                                <img src="./car brands/chevorlet.png" alt="chevorlet car" className='object-contain' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* div4 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>كيا</span>
-                                            <div className="max-w-16 max-h-16">
-                                                <img src="./car brands/kia.png" alt="kia car" className='object-contain' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* div5 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>نيسان</span>
-                                            <div className="max-w-16 max-h-16">
-                                                <img src="./car brands/nissan.png" alt="nissan car" className='object-contain' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* div6 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>فيات</span>
-                                            <div className="max-w-16 max-h-16">
-                                                <img src="./car brands/fiat.png" alt="fiat car" className='object-contain' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* div7 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>دايو</span>
-                                            <div className="max-w-16 max-h-16">
-                                                <img src="./car brands/daewoo.png" alt="daewoo car" className='object-contain' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* div8 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>سكودا</span>
-                                            <div className="max-w-16 max-h-16">
-                                                <img src="./car brands/skoda.png" alt="skoda car" className='object-contain' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* div9 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>بيجو</span>
-                                            <div className="max-w-16 max-h-16">
-                                                <img src="./car brands/peugeot.png" alt="peugeot car" className='object-contain' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* div10 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>ام جي</span>
-                                            <div className="max-w-16 max-h-16">
-                                                <img src="./car brands/mg.png" alt="mg car" className='object-contain' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* div11 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>سوزوكى</span>
-                                            <div className="max-w-16 max-h-16">
-                                                <img src="./car brands/suzuki.png" alt="suzuki car" className='object-contain' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* div12 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>بي ام دبليو</span>
-                                            <div className="max-w-16 max-h-16">
-                                                <img src="./car brands/bmw.png" alt="bmw car" className='object-contain' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <CarBrands/>
                             </div>
                         </div>
                     </div>
