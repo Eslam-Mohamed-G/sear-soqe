8ce6780a91622034d73b86fbde1dd54c8893a325
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-08-24 13:52:53 +0200

    add car shape component to BuyCar page

--- src/pages/BuyCar/BuyCar.jsx
+++ src/pages/BuyCar/BuyCar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import bgImg from '../../assets/image.png';
 import DropdownInput from '../../Component/InputDropDown/InputDropDown';
 import CarBrands from '../../Component/CarBrands/CarBrands';
+import CarShape from '../../Component/CarShape/CarShape';
 
 export default function BuyCar() {
     return (
@@ -40,50 +41,7 @@ export default function BuyCar() {
                         <div className="">
                             {/* محتوي تاب بتاعت شكل السيارة */}
                             <div className="w-full hidden">
-                                <div className="grid grid-cols-2 gap-2 mt-4">
-                                    {/* div1 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>سيدان</span>
-                                            <img src="./car shape/sedan.png" alt="sedan car" />
-                                        </div>
-                                    </div>
-                                    {/* div2 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>هاتشباك</span>
-                                            <img src="./car shape/hatchback.png" alt="hatchback car" />
-                                        </div>
-                                    </div>
-                                    {/* div3 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>مكشوفة</span>
-                                            <img src="./car shape/convertible.png" alt="convertible car" />
-                                        </div>
-                                    </div>
-                                    {/* div4 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>دفع رباعي</span>
-                                            <img src="./car shape/suv.png" alt="suv car" />
-                                        </div>
-                                    </div>
-                                    {/* div5 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>كوبيه</span>
-                                            <img src="./car shape/coupe.png" alt="coupe car" />
-                                        </div>
-                                    </div>
-                                    {/* div6 */}
-                                    <div className="overflow-hidden cursor-pointer customShadow_12 rounded-xl transition-all ease-in-out duration-500">
-                                        <div className="w-full flex flex-col items-end justify-between text-start px-2 py-2 md:px-4">
-                                            <span className='text-[11px] md:text-sm font-bold w-full'>بيك أب</span>
-                                            <img src="./car shape/pickup.png" alt="pickup car" />
-                                        </div>
-                                    </div>
-                                </div>
+                                <CarShape/>
                             </div>
 
                             {/* محتوي التاب بتاعت الماركة */}
