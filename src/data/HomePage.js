96fb961f92e6585080adc09c8b6cb69dece2f4d9
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-08-27 22:19:19 +0200

    add data file Contains all data for pages

---
+++ src/data/HomePage.js
@@ -0,0 +1,42 @@
+export const brands = [
+    { key: "hyundai", image: "./car brands/hyundai.png" },
+    { key: "toyota", image: "./car brands/tyota.png" },
+    { key: "chevrolet", image: "./car brands/chevorlet.png" },
+    { key: "kia", image: "./car brands/kia.png" },
+    { key: "nissan", image: "./car brands/nissan.png" },
+    { key: "fiat", image: "./car brands/fiat.png" },
+    { key: "daewoo", image: "./car brands/daewoo.png" },
+    { key: "skoda", image: "./car brands/skoda.png" },
+    { key: "peugeot", image: "./car brands/peugeot.png" },
+    { key: "mg", image: "./car brands/mg.png" },
+    { key: "suzuki", image: "./car brands/suzuki.png" },
+    { key: "bmw", image: "./car brands/bmw.png" },
+];
+
+export const carShapes = [
+    { key: "sedan", img: "./car shape/sedan.png", alt: "sedan car" },
+    { key: "hatchback", img: "./car shape/hatchback.png", alt: "hatchback car" },
+    { key: "convertible", img: "./car shape/convertible.png", alt: "convertible car" },
+    { key: "suv", img: "./car shape/suv.png", alt: "suv car" },
+    { key: "coupe", img: "./car shape/coupe.png", alt: "coupe car" },
+    { key: "pickup", img: "./car shape/pickup.png", alt: "pickup car" },
+];
+
+export const fuels = [
+    { key: "gasoline", img: "./fuel/gasoline.png" },
+    { key: "hybrid", img: "./fuel/hybrid.png" },
+    { key: "diesel", img: "./fuel/diesel.png" },
+    { key: "electric", img: "./fuel/electic.png" },
+];
+
+export const cities = [
+    { key: "cairo", image: "./fuel/skyscraper.png" },
+    { key: "mansoura", image: "./fuel/skyscraper.png" },
+    { key: "giza", image: "./fuel/skyscraper.png" },
+    { key: "alexandria", image: "./fuel/skyscraper.png" },
+    { key: "tanta", image: "./fuel/skyscraper.png" },
+    { key: "zagazig", image: "./fuel/skyscraper.png" },
+    { key: "sharqia", image: "./fuel/skyscraper.png" },
+    { key: "damietta", image: "./fuel/skyscraper.png" },
+    { key: "menoufia", image: "./fuel/skyscraper.png" },
+];
