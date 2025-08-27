cc10054ae97337d654f9e52cbb5e6b84cee100af
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-08-27 16:00:55 +0200

    add file translation to CarBrands components

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,9 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import enHomePage from "./locales/en/HomePageTranslation.json";
 import enNavbar from "./locales/en/navbarTranslation.json";
 import arNavbar from "./locales/ar/navbarTranslation.json";
+import arHomePage from "./locales/ar/HomePageTranslation.json";
 
 const saved = typeof window !== "undefined" ? localStorage.getItem("lang") : null;
 const browser = typeof navigator !== "undefined" ? navigator.language : "en";
@@ -11,12 +13,12 @@ i18n
     .use(initReactI18next)
     .init({
         resources: {
-            en: { navbar: enNavbar },
-            ar: { navbar: arNavbar },
+            en: { navbar: enNavbar, homePage: enHomePage },
+            ar: { navbar: arNavbar, homePage: arHomePage },
         },
         lng: fallback, // أول قيمة يعتمد عليها
         fallbackLng: "en",
-        ns: ["navbar"],
+        ns: ["navbar", "homePage"],
         defaultNS: "navbar",
         interpolation: { escapeValue: false },
     });
