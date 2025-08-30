780560b68d6e6a99da5749776edc7e63d7a88a74
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-08-30 13:36:38 +0200

    add translation to  register page

--- src/i18n.js
+++ src/i18n.js
@@ -6,6 +6,8 @@ import arNavbar from "./locales/ar/navbarTranslation.json";
 import arHomePage from "./locales/ar/HomePageTranslation.json";
 import arBuyCar from "./locales/ar/BuyCarPageTranslation.json";
 import enBuyCar from "./locales/en/BuyCarPageTranslation.json";
+import arRegister from "./locales/ar/registerPage.json";
+import enRegister from "./locales/en/registerPage.json";
 
 const saved = typeof window !== "undefined" ? localStorage.getItem("lang") : null;
 const browser = typeof navigator !== "undefined" ? navigator.language : "en";
@@ -15,12 +17,12 @@ i18n
     .use(initReactI18next)
     .init({
         resources: {
-            en: { navbar: enNavbar, homePage: enHomePage, buyCarPage: enBuyCar },
-            ar: { navbar: arNavbar, homePage: arHomePage, buyCarPage: arBuyCar },
+            en: { navbar: enNavbar, homePage: enHomePage, buyCarPage: enBuyCar, registerPage: enRegister},
+            ar: { navbar: arNavbar, homePage: arHomePage, buyCarPage: arBuyCar, registerPage: arRegister },
         },
         lng: fallback, // أول قيمة يعتمد عليها
         fallbackLng: "en",
-        ns: ["navbar", "homePage", "buyCarPage"],
+        ns: ["navbar", "homePage", "buyCarPage", "registerPage"],
         defaultNS: "navbar",
         interpolation: { escapeValue: false },
     });
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,38 +1,40 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 export default function Register() {
+    const { t } = useTranslation("registerPage");
     return (
         <div className="pt-28 px-4 sm:px-8 3xl:px-10 2xl:px-24">
             <form className="max-w-md mx-auto">
                 {/* name */}
                 <div className="relative z-0 w-full mb-5 group">
                     <input type="text" name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">name</label>
+                    <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{t("form.name")}</label>
                 </div>
                 {/* email */}
                 <div className="relative z-0 w-full mb-5 group">
                     <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                    <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{t("form.email")}</label>
                 </div>
                 {/* password */}
                 <div className="relative z-0 w-full mb-5 group">
                     <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                    <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{t("form.password")}</label>
                 </div>
                 {/* number */}
                 <div className="relative z-0 w-full mb-5 group">
                     <input type="tel" pattern="[0-9]{11}" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (010xxxxxxxx)</label>
+                    <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"> {t("form.phone")}</label>
                 </div>
                 {/* terms */}
                 <div className="flex items-start mb-5">
                     <div className="flex items-center h-5">
                         <input id="terms" type="checkbox" defaultValue className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
                     </div>
-                    <label htmlFor="terms" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
+                    <label htmlFor="terms" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{t("form.termsText")}<a href="#" className="text-blue-600 hover:underline dark:text-blue-500"> {t("form.termsLink")}</a></label>
                 </div>
 
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{t("form.submit")}</button>
             </form>
         </div>
     )
