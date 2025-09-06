aa773b1c95e2a044ec3f5b652c11c16510791783
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-09-06 11:15:13 +0200

    fetch data from back-end to show specific Offer

--- src/pages/SpecificCarAd/SpecificCarAd.jsx
+++ src/pages/SpecificCarAd/SpecificCarAd.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import axios from "axios";
 import Cookies from "js-cookie";
 
-export default function SpecificCarAd({id}) {
+export default function SpecificCarAd({ id }) {
     useEffect(() => {
         const specificOffer = async () => {
             try {
                 const cookieValue = Cookies.get("loginData");
                 if (!cookieValue) { return; }
                 const { token, user } = JSON.parse(cookieValue);
+                const response = await axios.get(`https://adminsand.alrmoz.com/api/user/listings/${id}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                            Accept: "application/json",
+                        }
+                    }
+                );
+                console.log(response);
             } catch (error) {
-
+                console.log(error);
             } finally {
 
             }
-        }
+        };
+
+        specificOffer();
     }, []);
     return (
         <div>SpecificCarAd</div>
