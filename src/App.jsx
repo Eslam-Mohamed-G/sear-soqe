5208dc22821efacbfc10524e32e846f4188318dd
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-09-07 14:06:50 +0200

    add CarGuide page

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ import Login from './pages/Login/Login';
 import StoreContextProvider from './Context/Context';
 import UserProfile from './pages/UserProfile/UserProfile';
 import SpecificCarAd from './pages/SpecificCarAd/SpecificCarAd';
+import CarGuide from './pages/carGuide/carGuide';
 
 const routes = createBrowserRouter([
   {path:"", element: <Layout/>, children:[
     { index: true, element: <HomePage/> },
     { path: 'buycar', element: <BuyCar/>},
     { path: 'sealcar', element: <SealCar/>},
+    { path: 'carGuide', element: <CarGuide/>},
     { path: 'register', element: <Register/>},
     { path: 'login', element: <Login/>},
     { path: '/profile', element: <UserProfile/>},
---
+++ src/pages/CarGuide/CarGuide.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CarGuide() {
+  return (
+    <div>دليل السيارات</div>
+  )
+}
